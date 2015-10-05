5035f4e8c222958555d6a457113019e1705de85c
duivvv <geoffrey.dhuyvetters@gmail.com>
2015-10-05 23:41:33 +0200

    trein

--- scripts/trein.js
+++ scripts/trein.js
@@ -16,7 +16,10 @@ module.exports = robot => {
         let departure = new Date(parseInt(data.connection[0].departure.time) * 1000);
         let arrival = new Date(parseInt(data.connection[0].arrival.time) * 1000);
 
-        let answer = `vertrek op spoor ${data.connection[0].departure.platform} om ${departure.getHours()}:${departure.getMinutes()} aankomst om ${arrival.getHours()}:${arrival.getMinutes()}`;
+        let vehicle = data.connection[0].departure.vehicle.split('.');
+        let direction = data.connection[0].departure.direction.name;
+
+        let answer = `(${vehicle[vehicle.length - 1]}, ${direction}) spoor ${data.connection[0].departure.platform}: ${departure.getHours()}:${departure.getMinutes()} -> ${arrival.getHours()}:${arrival.getMinutes()}`;
 
         return res.send(answer);
 
