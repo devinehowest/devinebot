b7d839b8c75b7871f2325932cf6fead37655cfb1
duivvv <geoffrey.dhuyvetters@gmail.com>
2015-10-06 15:53:30 +0200

    added asl

---
+++ scripts/asl.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let moment = require('moment');
+
+module.exports = (robot) => {
+
+  robot.hear(/asl/, res => {
+
+    let sex = 'R';
+    let location = 'Kortrijk';
+
+    let birth = '2015-10-05';
+
+    let years = moment().diff(birth, 'years');
+    let days = moment().diff(birth, 'days');
+
+    res.send(`${years} years & ${days} day(s) / ${sex} / ${location}`);
+
+  });
+
+};
