da880a8927c20dde9babfc4a42267282711ac704
duivvv <geoffrey.dhuyvetters@gmail.com>
2015-10-06 16:01:59 +0200

    finger

---
+++ scripts/pullmyfinger.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const pull = res => {
+  return res.send('as you wish, http://www.misternicehands.com/');
+};
+
+module.exports = (robot) => {
+
+  robot.respond(/finger/i, res => {
+    pull(res);
+  });
+
+  robot.respond(/vinger/i, res => {
+    pull(res);
+  });
+
+};
