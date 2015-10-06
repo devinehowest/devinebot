1d33ca8235497ce2b9b0d4c156269efe713cdaf5
duivvv <geoffrey.dhuyvetters@gmail.com>
2015-10-06 09:47:35 +0200

    trein

--- scripts/trein.js
+++ scripts/trein.js
@@ -2,28 +2,48 @@
 
 let fetch = require('node-fetch');
 
+const getTrein = (station, res) => {
+
+  let url = `http://api.irail.be/connections/?to=${station}&from=kortrijk&format=json`;
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+
+      let departure = new Date(parseInt(data.connection[0].departure.time) * 1000);
+      let arrival = new Date(parseInt(data.connection[0].arrival.time) * 1000);
+
+      let vehicle = data.connection[0].departure.vehicle.split('.');
+      let direction = data.connection[0].departure.direction.name;
+
+      let answer = `(${vehicle[vehicle.length - 1]}, ${direction}) spoor ${data.connection[0].departure.platform}: ${departure.getHours()}:${departure.getMinutes()} -> ${arrival.getHours()}:${arrival.getMinutes()}`;
+
+      return res.send(answer);
+
+    });
+
+}
+
 module.exports = robot => {
+
+  robot.hear(/trein/, res => {
+
+    let name = res.message.user.name.toLowerCase();
+
+    let obj = {
+      capens: 'sint-niklaas',
+      jynse: 'gent-sint-pieters',
+      geoffrey: 'gentbrugge',
+      shell: 'gentbrugge'
+    };
+
+    getTrein(obj[name], res);
+
+  });
 
   robot.hear(/trein (.*)/, res => {
 
-    let station = res.match[1];
-    let url = `http://api.irail.be/connections/?to=${station}&from=kortrijk&format=json`;
-
-    fetch(url)
-      .then(res => res.json())
-      .then(data => {
-
-        let departure = new Date(parseInt(data.connection[0].departure.time) * 1000);
-        let arrival = new Date(parseInt(data.connection[0].arrival.time) * 1000);
-
-        let vehicle = data.connection[0].departure.vehicle.split('.');
-        let direction = data.connection[0].departure.direction.name;
-
-        let answer = `(${vehicle[vehicle.length - 1]}, ${direction}) spoor ${data.connection[0].departure.platform}: ${departure.getHours()}:${departure.getMinutes()} -> ${arrival.getHours()}:${arrival.getMinutes()}`;
-
-        return res.send(answer);
-
-      });
+    getTrein(res.match[1], res);
 
   });
 
