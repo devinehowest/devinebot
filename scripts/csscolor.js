046565b450afbfb9bf60fc1a2602aa2beefffff8
duivvv <geoffrey.dhuyvetters@gmail.com>
2015-10-11 17:00:36 +0200

    bold text

--- scripts/csscolor.js
+++ scripts/csscolor.js
@@ -6,7 +6,7 @@ module.exports = robot => {
 
   robot.respond(/csscolor/i, res => {
     let color = colors[Math.round(Math.random() * (colors.length-1))];
-    return res.send(`${color.name} ${color.hex}`);
+    return res.send(`*${color.name}* ${color.hex}`);
   });
 
 };
